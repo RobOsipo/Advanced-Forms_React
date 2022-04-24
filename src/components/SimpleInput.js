808e48bec970915bbdacbd25f1361064829c3477
Robert <osipovitchr@gmail.com>
2022-04-24 13:41:14 -0400

    refactor e.target.value to ref.curernt.value

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,9 +1,27 @@
+import React, {useRef, useState} from 'react'
+
 const SimpleInput = (props) => {
+
+    const nameInputRef = useRef(null)
+    const [enteredName, setEnteredName] = useState('')
+
+    const nameInputChangeHandler = (e) => {
+        setEnteredName(e.target.value)
+    }
+
+    const formSubmissionHandler = (e) => {
+        e.preventDefault()
+
+        const enteredValue = nameInputRef.current.value
+        console.log(enteredValue)
+        setEnteredName('')
+    }
+    
     return (
-      <form>
+      <form onSubmit={formSubmissionHandler}>
         <div className='form-control'>
           <label htmlFor='name'>Your Name</label>
-          <input type='text' id='name' />
+          <input ref={nameInputRef} value={enteredValue} type='text' id='name' onChange={nameInputChangeHandler} />
         </div>
         <div className="form-actions">
           <button>Submit</button>
